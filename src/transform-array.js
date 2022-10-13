777880a00a6bb68c01128d3b7e49ec319c685d1f
ilya <shakurov.ia@gmail.com>
2022-10-13 17:07:56 +0300

    solution dream-team

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,11 +13,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
+function transform(arr) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  // if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!");
+  // if (!arr.length) return [];
+  // let result = [];
+
+  // for (let i = 0; i < arr.length; i++) {
+  //   if (i === '--discard-next') {
+  //     i++;
+  //     continue;
+  //   } else if (i === '--discard-prev') {
+  //     if (i !== 0 && arr[i - 2] !== '--discard-next') result.pop();
+  //     continue;
+  //   } else if (i === '--double-next ') {
+  //     if (arr[i + 1]) result.push(arr[i + 1]);
+  //     continue;
+  //   } else if (i === '--double-prev') {
+  //     if (i !== 0 && arr[i - 2] !== '--discard-next') result.push(arr[i - 1]);
+  //     continue;
+  //   } else {
+  //     result.push(arr[i]);
+  //   }
+  // }
+  // return result;
 }
 
 module.exports = {
   transform
 };
+
+// transform([1, 2, 3, '--double-next', 4, 5]);
+// transform([1, 2, 3, '--discard-prev', 4, 5]);
