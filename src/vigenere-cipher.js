1f9a8900afdc12eacc1bac3c7d58b67f1f06449c
ilya <shakurov.ia@gmail.com>
2022-10-14 16:30:55 +0300

    solution vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,54 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   constructor(direct = true) {
+    this.direct = direct;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ';
+   }
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    let result = [];
+    let letterIndex = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (this.alphabet.includes(message[i].toUpperCase())) {
+        let position = this.alphabet.indexOf(message[i].toUpperCase());
+        let shift = this.alphabet.indexOf(key[letterIndex % key.length].toUpperCase());
+        result.push(this.alphabet[position + shift]);
+        letterIndex++;
+      } else {
+        result.push(message[i]);
+      }
+    }
+
+    return this.direct ? result.join('') : result.reverse().join('');
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  
+  decrypt(encryptedMessage, key) {
+    if (!encryptedMessage || !key) throw new Error('Incorrect arguments!');
+
+    let result = [];
+    let letterIndex = 0;
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      if (this.alphabet.includes(encryptedMessage[i])) {
+        let position = this.alphabet.indexOf(encryptedMessage[i], 26);
+        let shift = this.alphabet.indexOf(key[letterIndex % key.length].toUpperCase());
+        result.push(this.alphabet[position - shift]);
+        letterIndex++;
+      } else {
+        result.push(encryptedMessage[i]);
+      }
+    }
+
+    return this.direct ? result.join('') : result.reverse().join('');
   }
 }
 
 module.exports = {
   VigenereCipheringMachine
 };
+
